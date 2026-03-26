e5cbc683dfd4b21d98ac99777687d2d4ab3ad4a5
Dorian-102 <petit.dorian12@gmail.com>
2026-03-26 12:12:09 +0100

    DP - Commit 6

--- app.py
+++ app.py
@@ -1,21 +1,48 @@
-from dash import Dash
+from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
+from dash import Input, Output, callback
 import sys
 import os
 
-# Ajout de la racine au path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-#from pages import page1
-#from pages import page1_cb
-# from pages import page2_cb
-# from pages import page2
+from pages import page1, page1_cb
+from pages import page2, page2_cb
 from pages import page3
-#from pages import page3_cb
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = page3.layout
+# Layout principal avec navigation
+app.layout = html.Div([
+    # Barre de navigation
+    dbc.NavbarSimple(
+        children=[
+            dbc.NavItem(dcc.Link("Comparaison entre régions", href="/page1", className="nav-link text-white")),
+            dbc.NavItem(dcc.Link("Affichage des données", href="/page2", className="nav-link text-white")),
+            dbc.NavItem(dcc.Link("Aide en ligne", href="/page3", className="nav-link text-white")),
+        ],
+        brand="Application des M1 MECEN",
+        brand_style={"color": "white"},
+        color="#1a6fc4",
+        dark=True,
+    ),
+
+    # Contenu de la page
+    dcc.Location(id="url", refresh=False),
+    html.Div(id="page-content")
+])
+
+@callback(
+    Output("page-content", "children"),
+    Input("url", "pathname")
+)
+def display_page(pathname):
+    if pathname == "/page2":
+        return page2.layout
+    elif pathname == "/page3":
+        return page3.layout
+    else:
+        return page1.layout
 
 if __name__ == '__main__':
     app.run(debug=True)
